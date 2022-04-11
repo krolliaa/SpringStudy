2c9705c1fda27f6abbaf879900f2d09f551816ca
krolliaa <krolliaa@163.com>
2022-04-11 14:15:08 +0800

    AOP 定义切入点

--- Spring.md
+++ Spring.md
@@ -1627,3 +1627,27 @@ public class MyAfterAspect {
 }
 ```
 
+#### 2.4.6 定义切入点
+
+```java
+package com.zwm.aspect;
+
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyPointCut {
+    @AfterThrowing(value = "myPointcut()")
+    public void myAfterThrowing() {
+        System.out.println("最终通知：最终总是会执行的方法");
+    }
+
+    @Pointcut(value = "execution(* com.zwm.service.impl.SomeServiceImpl.doSome(..))")
+    public void myPointcut() {
+    }
+}
+```
+
---
+++ src/main/java/com/zwm/App18.java
@@ -0,0 +1,16 @@
+package com.zwm;
+
+import com.zwm.service.SomeService;
+import com.zwm.service.impl.SomeServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App18 {
+    public static void main(String[] args) {
+        String springConfig = "applicationContext18.xml";
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(springConfig);
+        SomeService someService = (SomeServiceImpl) applicationContext.getBean("someServiceImpl");
+        Object result = (Object) someService.doSome("ABC", 3);
+        System.out.println(result);
+    }
+}
